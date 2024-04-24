95bc140dcec6ff431d44b1b3db3f6c774f88c140
Bryan Dalton <bryantdalton19@gmail.com>
2024-04-24 11:55:33 -0400

    initial commit

---
+++ src/header.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+function header() {
+  return (
+    <div id="header"><h1>&lt;Web Design By: Bryan Dalton /&gt; </h1>
+    </div>
+  );
+}
+
+export default header;
---
+++ src/project.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+
+export default function Project(props) {
+  return (
+    <div className='project'>
+      <a className='projectLinks' href={props.link}>
+        <img src={props.img} alt={props.name}/>
+        <p>{props.name}</p>
+     </a>
+     <a className='projectLinks' href={props.github}>
+        <img id='git' alt='Github' src='./images/github-white.png'></img>
+      </a>
+      <p className='type'>{props.type}</p>
+      <p className='description'>{props.description}</p>
+    </div>
+  )
+}
